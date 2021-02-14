cff390bbf1232a74890ba99779940112357601b3
Ricardo Holmes <ricardoholmes2004@gmail.com>
2021-02-14 08:13:29 -0600

    No Comments

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -7,23 +7,24 @@ public class CameraMovement : MonoBehaviour
     [Range(0, 1)]
     public float smoothSpeed;
     public Transform target;
-    float relX;
+    float relZ;
 
     Vector3 velocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        relX = target.position.x - transform.position.x;
+        relZ = target.position.z - transform.position.z;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Vector3 targetPos = transform.position;
-        targetPos.x = target.position.x - relX;
+        targetPos.z = target.position.z - relZ;
 
-        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothSpeed);
-        transform.position = smoothPosition;
+        //Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothSpeed);
+        //transform.position = smoothPosition;
+        transform.position = targetPos;
     }
 }
