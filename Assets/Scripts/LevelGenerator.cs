d3a6582e5215ac3e921c0c69671144f44edd189d
Hesam Zakeri <hesam.zk.81@gmail.com>
2021-02-14 08:58:38 -0600

    No Comments

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public float triggerDistance = 200f;
-    public Transform player;
+    [SerializeField] private Transform firstLevel;
+    [SerializeField] private Transform nextLevel;
+    [SerializeField] private Transform player;
+
+    private float triggerDistance = 50f;
+
+    //public Transform player;
+    private Vector3 lastEndPosition;
     // Start is called before the first frame update
     void Start()
     {
-        //lastEndPosition = 
+        lastEndPosition = firstLevel.Find("EndPosition").position;
+        SpawnLevel();
+        SpawnLevel();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Vector3.Distance(player, lastEndPosition) < triggerDistance)
-        //{
+        if (Vector3.Distance(player.position, lastEndPosition) < triggerDistance)
+        {
 
-        //}
+        }
+    }
+
+    private void SpawnLevel()
+    {
+        Transform lastLevelPartTransform = SpawnLevel(lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
+    }
+
+    private Transform SpawnLevel(Vector3 spawnPosition)
+    {
+        Transform newLevelTransform = Instantiate(nextLevel, spawnPosition, Quaternion.identity);
+        return newLevelTransform;
     }
 }
