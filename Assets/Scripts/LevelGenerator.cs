abde6cf35e76b97c83be868873f189e4ee2ed56f
Hesam Zakeri <hesam.zk.81@gmail.com>
2021-02-14 08:15:11 -0600

    No Comments

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public float triggerDistance = 200f;
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Vector3.Distance(player.GetPosition(), ))
+    }
+}
