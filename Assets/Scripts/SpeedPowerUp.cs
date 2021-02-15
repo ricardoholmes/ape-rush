6b12bfefa1236dd1b345743e098678151f390e64
Hesam Zakeri <hesam.zk.81@gmail.com>
2021-02-15 14:37:25 -0600

    No Comments

--- Assets/Scripts/SpeedPowerUp.cs
+++ Assets/Scripts/SpeedPowerUp.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class SpeedPowerUp : MonoBehaviour
 {
-    public float acceleration;
-    public Rigidbody player;
-
-    void OnTriggerEnter(Collider collider)
-    {
-        if (collider.CompareTag("Player"))
-        {
-            player.AddForce(Vector3.right * acceleration, ForceMode.Acceleration);
-            Destroy(gameObject);
-        }
-    }
+    
 }
