b5e141d85787ada9f131135d4b96f337191bc0c4
Ricardo Holmes <ricardoholmes2004@gmail.com>
2021-02-16 12:44:58 -0600

    No Comments

--- Assets/Scripts/NextPlatform.cs
+++ Assets/Scripts/NextPlatform.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class nextPlatform : MonoBehaviour
+public class NextPlatform : MonoBehaviour
 {
     void Update()
     {
