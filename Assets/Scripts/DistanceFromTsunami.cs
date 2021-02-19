788d9571713df86d0faf56004261ea25fd45deec
Hesam Zakeri <hesam.zk.81@gmail.com>
2021-02-19 10:29:15 -0600

    No Comments

--- Assets/Scripts/DistanceFromTsunami.cs
+++ Assets/Scripts/DistanceFromTsunami.cs
@@ -16,11 +16,11 @@ public class DistanceFromTsunami : MonoBehaviour
     {
         if (int.Parse(textComponent.text.Split('m')[0]) <= redTrigger)
         {
-            textComponent.color = new Color32(255, 0, 0, 255);
+            textComponent.color = new Color32(210, 0, 0, 255);
 
         } else
         {
-            textComponent.color = new Color32(0, 0, 0, 255);
+            textComponent.color = new Color32(0, 65, 101, 255);
         }
     }
 }
