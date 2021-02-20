0f6cd94cb18f570fa738a3ec9db53f8edbd2871d
Ricardo Holmes <ricardoholmes2004@gmail.com>
2021-02-20 06:46:15 -0600

    No Comments

---
+++ Assets/Scripts/DontMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontMove : MonoBehaviour
+{
+    Vector3 localPos;
+
+    public bool ignoreX;
+    public bool ignoreY;
+    public bool ignoreZ;
+
+    void Awake()
+    {
+        localPos = transform.localPosition;
+    }
+
+    void Update()
+    {
+        Vector3 pos = new Vector3
+        {
+            x = ignoreX ? transform.localPosition.x : localPos.x,
+            y = ignoreY ? transform.localPosition.y : localPos.y,
+            z = ignoreZ ? transform.localPosition.z : localPos.z
+        };
+        transform.localPosition = pos;
+    }
+}
